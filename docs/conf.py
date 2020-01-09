42a92d671abe75cf15e31333eb1b7ff861da7e89
MUJI <jycycwy@sina.com>
2020-01-09 19:27:43 +0800

    修改conf文件

---
+++ docs/conf.py
@@ -0,0 +1,17 @@
+# @IDE: PyCharm
+# @Author: Allen
+# @File: conf.py
+# @Time: 2020/1/9 19:00
+
+from recommonmark.parser import CommonMarkParser
+
+source_parsers = {
+    '.md':CommonMarkParser,
+    '.markdown':CommonMarkParser
+}
+source_suffix = ['.rst', '.md', '.markdown']
+
+extensions = []
+
+
+
