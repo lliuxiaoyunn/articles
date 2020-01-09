80018f3d0907894d36ab22342d14d1656f822632
MUJI <jycycwy@sina.com>
2020-01-09 22:12:40 +0800

    更改配置文件

--- docs/conf_1.py
+++ docs/conf_1.py
@@ -3,15 +3,17 @@
 # @File: conf.py
 # @Time: 2020/1/9 19:00
 
+
 from recommonmark.parser import CommonMarkParser
+
 
 source_parsers = {
     '.md':CommonMarkParser,
-    '.markdown':CommonMarkParser
 }
 source_suffix = ['.rst', '.md', '.markdown']
 
 extensions = []
+
 
 
 
